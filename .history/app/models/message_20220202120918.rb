df826b54fa99b6d848cf112d9882d8cc6ee37706
xso2 <odaken1002@gmail.com>
2022-02-02 12:13:34 +0900

    空のメッセージが送信できないバリデーションの設定

--- .history/app/models/message_20220202120918.rb
+++ .history/app/models/message_20220202120918.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room #1つのメッセージは、1つのチャットルームに存在します
   belongs_to :user #1つのメッセージは、1人のユーザーから送信されます。
 
-  validates :content, presence: true #空の場合はDBに保存しないというバリデーション
+  validates :content, presence: true
+end
+
 end

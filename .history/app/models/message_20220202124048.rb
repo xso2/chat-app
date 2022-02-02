1d9836a462891b5673f1e680a408924f894e2153
xso2 <odaken1002@gmail.com>
2022-02-02 12:42:26 +0900

    Active Storageの導入

--- .history/app/models/message_20220202124048.rb
+++ .history/app/models/message_20220202124048.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room #1つのメッセージは、1つのチャットルームに存在します
   belongs_to :user #1つのメッセージは、1人のユーザーから送信されます。
-  has_one_attached :image #レコードとファイルを1対1の関係で紐づけるメソッドです。
+  has_one_attached :image 
 
   validates :content, presence: true #空の場合はDBに保存しないというバリデーション
 end

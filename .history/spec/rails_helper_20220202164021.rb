5578b9f137cc245eb3f7cc1977e26fe0172f45cd
xso2 <odaken1002@gmail.com>
2022-02-02 17:00:09 +0900

    結合テストの実装

--- .history/spec/rails_helper_20220202164021.rb
+++ .history/spec/rails_helper_20220202164021.rb
@@ -36,7 +36,7 @@ I18n.locale = "en"
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include SignInSupport
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
